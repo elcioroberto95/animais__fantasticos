b579696aeae324a5370fa6fff775107fbf376bb2
Elcio <elcioroberto1995@gmail.com>
2020-10-17 20:44:33 -0300

    tooltip refatorar

--- js/script.js
+++ js/script.js
@@ -3,12 +3,13 @@ import initAnimacaoScroll from './modules/scroll-animacao.js';
 import Accordion from './modules/accordion.js';
 import TabNav from './modules/tabnav.js';
 import Modal from './modules/modal.js';
-import initTooltip from './modules/tooltip.js';
+import Tooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
+
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -22,11 +23,11 @@ modal.init();
 
 
 
+const toltip = new Tooltip('[data-tooltip]');
+toltip.init();
 
 
 
-
-initTooltip();
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
