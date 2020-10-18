8d1b590c3b6604cfe94402bac05883126aae67b2
Elcio <elcioroberto1995@gmail.com>
2020-10-18 20:14:34 -0300

    funcionamento refatorado

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ import TabNav from './modules/tabnav.js';
 import Modal from './modules/modal.js';
 import Tooltip from './modules/tooltip.js';
 import DropdownMenu from './modules/dropdown-menu.js';
-import initFuncionamento from './modules/funcionamento.js';
+import Funcionamento from './modules/funcionamento.js';
 import fetchAnimais from './modules/fetch-animais.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
 import MenuMobile from './modules/menu-mobile.js';
@@ -13,33 +13,35 @@ import MenuMobile from './modules/menu-mobile.js';
 
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
-scrollSuave.init();
+    scrollSuave.init();
+
 const accordion = new Accordion('[data-anime="accordion"] dt')
-accordion.init();
+    accordion.init();
+
 const tabNav =  new TabNav('[data-tab="menu"] li','[data-tab="content"] section');
-tabNav.init();
+    tabNav.init();
 
 const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
-modal.init();
-
-
+    modal.init();
 
 const toltip = new Tooltip('[data-tooltip]');
-toltip.init();
+    toltip.init();
 
 
 const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
-scrollAnima.init();
-fetchAnimais('../../animaisapi.json','.numeros-grid');
-fetchBitcoin('https://blockchain.info/ticker','.btc-preco');
+    scrollAnima.init();
+
 
 const dropdownMenu = new DropdownMenu('[data-dropdown]');
-dropdownMenu.init();
+    dropdownMenu.init();
 
 const menuMobile = new MenuMobile('[data-menu="button"]','[data-menu="list"]');
-menuMobile.init();
-initFuncionamento();
+    menuMobile.init();
 
+const funcionamento = new Funcionamento('[data-semana]','aberto');
+funcionamento.init();
+fetchAnimais('../../animaisapi.json','.numeros-grid');
 
+fetchBitcoin('https://blockchain.info/ticker','.btc-preco');
 
 
